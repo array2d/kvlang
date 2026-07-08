afcd6b2824e4d26077eceb7d4d8dec7f56f7ecf5
peng.li24 <734991033@qq.com>
2026-07-08 03:10:42 +0000

    refactor(cli): extract showHelp to help.go

---
+++ cmd/kvlang/help.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func showHelp() {
+	fmt.Fprintln(os.Stderr, "kvlang — KV language VM interpreter")
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr, "usage: kvlang [flags] [<file.kv>]")
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr, "commands:")
+	fmt.Fprintln(os.Stderr, "  help              show this help")
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr, "flags:")
+	fmt.Fprintln(os.Stderr, "  -c \"code\"          execute inline code")
+	fmt.Fprintln(os.Stderr, "  -h, --help        show this help")
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr, "examples:")
+	fmt.Fprintln(os.Stderr, "  kvlang                    start daemon")
+	fmt.Fprintln(os.Stderr, "  kvlang file.kv            execute file")
+	fmt.Fprintln(os.Stderr, "  kvlang -c '1+2->./x'     inline code")
+	fmt.Fprintln(os.Stderr, "  echo 'code' | kvlang      pipe input")
+	os.Exit(0)
+}
